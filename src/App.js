c64aed62e8a8dea41c24392960da93688eadaa7b
Mtanveerkhan <tannokhan@hotmail.com>
2021-10-18 22:48:13 +0500

    Code completed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,58 @@ export default function App() {
   return (
     <div>
       <h1>HELLO WORLD</h1>
+	<div className="table-settings mb-4">
+    <Row className="justify-content-between align-items-center">
+        <Col xs={9} lg={4} className="d-flex">
+            <InputGroup className="me-2 me-lg-3">
+                <InputGroup.Text>
+                    <FontAwesomeIcon icon={faSearch} />
+                </InputGroup.Text>
+                <Form.Control type="text" placeholder="Search" />
+            </InputGroup>
+            <Form.Select className="w-25">
+                <option defaultChecked>All</option>
+                <option value="1">Active</option>
+                <option value="2">Inactive</option>
+                <option value="3">Pending</option>
+                <option value="3">Canceled</option>
+            </Form.Select>
+        </Col>
+        <Col xs={3} lg={8} className="text-end">
+            <Dropdown as={ButtonGroup} className="me-2">
+                <Dropdown.Toggle split as={Button} variant="link" className="text-dark m-0 p-0">
+                    <span className="icon icon-sm icon-gray">
+                        <FontAwesomeIcon icon={faSlidersH} />
+                    </span>
+                </Dropdown.Toggle>
+                <Dropdown.Menu className="dropdown-menu-right">
+                    <Dropdown.Item className="fw-bold text-dark">Show</Dropdown.Item>
+                    <Dropdown.Item className="d-flex fw-bold">
+                        10 <span className="icon icon-small ms-auto"><FontAwesomeIcon icon={faCheck} /></span>
+                    </Dropdown.Item>
+                    <Dropdown.Item className="fw-bold">20</Dropdown.Item>
+                    <Dropdown.Item className="fw-bold">30</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+            <Dropdown as={ButtonGroup}>
+                <Dropdown.Toggle split as={Button} variant="link" className="text-dark m-0 p-0">
+                    <span className="icon icon-sm icon-gray">
+                        <FontAwesomeIcon icon={faCog} />
+                    </span>
+                </Dropdown.Toggle>
+                <Dropdown.Menu className="dropdown-menu-right">
+                    <Dropdown.Item className="fw-bold text-dark">Show</Dropdown.Item>
+                    <Dropdown.Item className="d-flex fw-bold">
+                        10 <span className="icon icon-small ms-auto"><FontAwesomeIcon icon={faCheck} /></span>
+                    </Dropdown.Item>
+                    <Dropdown.Item className="fw-bold">20</Dropdown.Item>
+                    <Dropdown.Item className="fw-bold">30</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+        </Col>
+    </Row>
+</div>
+    
     </div>
   )
 }
